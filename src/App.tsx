4c75a4bb8e8201c92f2287d568745769fc56a9a4
Vlad Snow <vlad.snow@bbconsult.co.uk>
2022-01-15 16:07:58 +0200

    add todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React, {useEffect, useState} from 'react';
 import Card, {CardVariation} from "./components/Card";
 import UserList from "./components/UserList";
-import {IUser} from "./types/types";
+import {ITodo, IUser} from "./types/types";
 import axios from "axios";
+import List from "./components/List";
+import UserItem from "./components/UserItem";
+import TodoItem from "./components/TodoItem";
 
 const App = () => {
     const [users, setUsers] = useState<IUser[]>([]);
+    const [todos, setTodos] = useState<ITodo[]>([]);
 
     useEffect(() => {
         fetchUsers();
+        fetchTodos();
     }, [])
 
     const fetchUsers = async () => {
@@ -20,13 +25,29 @@ const App = () => {
         }
     }
 
+    const fetchTodos = async () => {
+        try {
+            const response = await axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10');
+            setTodos(response.data);
+        } catch (e) {
+            alert(e)
+        }
+    }
+
     return (
         <div>
-          <Card onClick={(num) => console.log(num)} width='200px' height='200px' variant={CardVariation.outlined}>
-              <button>Button</button>
-              <div>test</div>
-          </Card>
-          <UserList users={users}/>
+            <Card onClick={(num) => console.log(num)} width='200px' height='200px' variant={CardVariation.outlined}>
+                <button>Button</button>
+                <div>test</div>
+            </Card>
+            <List
+                items={users}
+                renderItem={(user: IUser) => <UserItem key={user.id} user={user}/>}
+            />
+            <List
+                items={todos}
+                renderItem={(todo: ITodo) => <TodoItem key={todo.id} todo={todo}/>}
+            />
         </div>
     );
 };
