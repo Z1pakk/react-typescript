b4c251e45e55bf8fcadba6510cc124cee4bea3fb
Vlad Snow <vlad.snow@bbconsult.co.uk>
2022-01-15 15:53:34 +0200

    add mock users

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import Card, {CardVariation} from "./components/Card";
+import UserList from "./components/UserList";
+import {IUser} from "./types/types";
 
 const App = () => {
+    const users: IUser[] = [
+        {id: 1, name: 'Vlad', email: "awdawd@gmail.com", address: {city: 'Rivne', street: 'Soborna', zipcode: '33028'}},
+        {id: 2, name: 'Test', email: "test@gmail.com", address: {city: 'Rivne', street: 'Soborna2', zipcode: '33067'}}
+    ]
+
     return (
         <div>
           <Card onClick={(num) => console.log(num)} width='200px' height='200px' variant={CardVariation.outlined}>
               <button>Button</button>
-              awdawd
+              <div>test</div>
           </Card>
+          <UserList users={users}/>
         </div>
     );
 };
