1de310617c6451f0e75265d5b3295cc5b6a59606
Vlad Snow <vlad.snow@bbconsult.co.uk>
2022-01-16 21:26:20 +0200

    Detailed example with typed react events

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import List from "./components/List";
 import UserItem from "./components/UserItem";
 import TodoItem from "./components/TodoItem";
+import EventsExample from "./components/EventsExample";
 
 const App = () => {
     const [users, setUsers] = useState<IUser[]>([]);
@@ -36,6 +37,7 @@ const App = () => {
 
     return (
         <div>
+            <EventsExample />
             <Card onClick={(num) => console.log(num)} width='200px' height='200px' variant={CardVariation.outlined}>
                 <button>Button</button>
                 <div>test</div>
---
+++ src/components/EventsExample.tsx
@@ -0,0 +1,52 @@
+import React, {FC, useRef, useState} from 'react';
+
+const EventsExample: FC = () => {
+    const [value, setValue] = useState<string>('');
+    const [isDrag, setIsDrag] = useState<boolean>(false);
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+    }
+
+    const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+        console.log(inputRef.current?.value)
+    }
+
+    const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        console.log('Drag')
+    }
+
+    const dragWithPreventHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setIsDrag(true);
+    }
+
+    const leaveHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setIsDrag(false);
+        console.log('Dropped')
+    }
+
+    const dropHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setIsDrag(false);
+    }
+
+    return (
+        <div>
+            <input value={value} onChange={changeHandler} type="text" placeholder='Managed'/>
+            <input ref={inputRef} type="text" placeholder='Not managed'/>
+            <button onClick={clickHandler}>show</button>
+            <div onDrag={dragHandler} draggable style={{width: 200, height: 200, background: 'red'}}></div>
+            <div
+                onDrop={dropHandler}
+                onDragLeave={leaveHandler}
+                onDragOver={dragWithPreventHandler}
+                style={{width: 200, height: 200, background: isDrag ? 'blue' : 'red', marginTop: 15}}
+            ></div>
+        </div>
+    );
+};
+
+export default EventsExample;
